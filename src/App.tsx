589dce379c7f2c27dd0a42fe5befd14cd5227a8f
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-12-03 09:39:28 +0100

    algo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function repartitionEquitable(tab: number[], nb: number): number[][] {
+    const newTab: number[][] = [];
+    for (let i = 0; i < tab.length; i += nb) {
+      newTab.push(tab.slice(i, i + nb));
+    }
+
+    for (let k = 0; k < newTab.length; k++) {
+      if (newTab[k].length !== nb) {
+        const extraElements = newTab[k];
+        newTab.splice(k, 1); 
+        extraElements.forEach(element => {
+          const randomIndex = Math.floor(Math.random() * newTab.length);
+          newTab[randomIndex].push(element);
+        });
+      }
+    }
+
+    return newTab;
+  }
+
+  console.log(repartitionEquitable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+  console.log(repartitionEquitable([0, 1, 0, 1, 0, 1, 0, 1], 3));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data Table</h1>
     </div>
   );
 }
